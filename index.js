fae329e4f9625443f4f0e2418cf33e0c2d8a4526
Your Name <kawaiineko55@gmail.com>
2024-03-26 12:23:58 +0000

    lesson 31

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import { name } from "./module";
-import "./styles.css";
-console.log("Your name is: " + name);
+// import { name } from "./module";
+// import "./styles.css";
+// import isEmail from "validator/lib/isEmail";
+// import isURL from "validator/lib/isURL";
+// import toDate from "validator/lib/toDate";
+
+// console.log("Your name is: " + name);
+
+// console.log(isEmail("foo@bar.com")); //=> true
+// console.log(
+//   isURL("https://github.com/JuliaT47/JSHomework/blob/main/dist/index.html")
+// );
+// console.log(toDate("12 12 2000"));
+
+// var moment = require("moment");
+
+// console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
+// console.log(moment().format("MMM Do YY"));
